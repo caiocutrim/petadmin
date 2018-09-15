4f58ad2873569517ed7743132c5d808c3095b35a
caio cutrim <caiocutrim.js@gmail.com>
2018-09-15 10:40:02 -0300

    Finish campain and jobs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,11 +30,16 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # Host and port of current system
+  # For development enviroment we are using gem called mailcatcher, refer to google for more info
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1080 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              '127.0.0.1',
+    port:                 1025,
+  }
 
   config.action_mailer.perform_caching = false
 
